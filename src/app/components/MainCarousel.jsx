f4a847c26803a46bf9730927aa8eaae55414593c
oechristophers <oechristophers2020@gmail.com>
2024-03-09 23:32:21 +0100

    fix

--- src/app/components/MainCarousel.jsx
+++ src/app/components/MainCarousel.jsx
@@ -11,15 +11,34 @@ export function EmblaCarousel() {
     <div className="embla" ref={emblaRef}>
       <div className="embla__container">
         <div className="embla__slide1">
-          <section className="font-bold text-5xl text-transparent bg-gradient-to-b from-green-600 via-green-200 to-green-100 bg-clip-text h-[100vh] "></section>
+          <section className="font-bold text-5xl text-transparent bg-gradient-to-b from-green-600 via-green-200 to-green-100 bg-clip-text h-[100vh] ">
+            <Image 
+            src='/frontcollage.webp'
+            alt="slide"
+            layout="responsive"
+            width={500}
+            height={400}/>
+          </section>
         </div>
         <div className="embla__slide2">
           <section className="font-bold text-5xl text-transparent bg-gradient-to-b from-green-600 via-green-200 to-green-100 bg-clip-text h-[100vh] ">
-             
+            <Image 
+            src='/frontcollage2.webp'
+            alt="slide"
+            layout="responsive"
+            width={500}
+            height={400}/>
           </section>
         </div>
         <div className="embla__slide3 bg-blue-950 ml-12">
-          <section className="font-bold text-5xl text-transparent bg-gradient-to-b from-yellow-500 via-yellow-200 to-yellow-100 bg-clip-text h-[100vh] ml-3 w-[100vw]"></section>
+          <section className="font-bold text-5xl text-transparent bg-gradient-to-b from-yellow-500 via-yellow-200 to-yellow-100 bg-clip-text h-[100vh] w-[100vw]">
+            <Image 
+            src='/frontcollage3.webp'
+            alt="slide"
+            layout="responsive"
+            width={500}
+            height={400}/>
+          </section>
         </div>
       </div>
     </div>
