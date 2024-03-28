12a798b7d597477730f2b3d1a28c1e06c07fe043
oechristophers <oechristophers2020@gmail.com>
2024-03-28 18:07:40 +0100

    fix

--- src/app/shop/cart/CartButton.jsx
+++ src/app/shop/cart/CartButton.jsx
@@ -1,43 +1,42 @@
-import React, { useEffect } from 'react'
-import { useCart } from './Cart';
+import React, { useEffect } from "react";
+import { useCart } from "./Cart";
 
-export default function CartButton({prodCard, params,product}) {
-      const { quant, setQuant } = useCart();
-      const { addToCart } = useCart();
-       function handleQuant(e) {
-         setQuant(e.target.value);
-       }
+export default function CartButton({ prodCard, params, product }) {
+  const { quant, setQuant } = useCart();
+  const { addToCart } = useCart();
+  function handleQuant(e) {
+    setQuant(e.target.value);
+  }
 
-         useEffect(() => {
-           setQuant(1); // this is to Reset quant state to default value
-         }, [params.product]);
+  useEffect(() => {
+    setQuant(1); // this is to Reset quant state to default value
+  }, [params.product]);
 
   return (
     <section>
-   <div className="flex pb-6">
-              <p>Quantity: &nbsp;</p>
-              <span className="">
-              
-                <input
-                  type="number"
-                  name="quant"
-                  id="quant"
-                  min={1}
-                  max={prodCard.stock}
-                  value={quant}
-                  className="bg-green-300"
-                  onChange={handleQuant}
-                />
-              </span>
-            </div>
-            <div>
-              <button
-                onClick={() => addToCart(prodCard)}
-                className="border-2 w-[70%] h-14 mb-5"
-              >
-                Add to cart
-              </button>
-            </div>
-            </section>
-  )
+      <div className="flex pb-6">
+        <p>Quantity: &nbsp;</p>
+        <span className="">
+          <input
+            type="number"
+            name="quant"
+            id="quant"
+            min={1}
+            max={prodCard.stock}
+            value={quant}
+            className="max-w-10"
+            onChange={handleQuant}
+          />
+        </span>
+      </div>
+      <div>
+        <button
+          onClick={() => addToCart(prodCard)}
+          className="border-2 w-[70%] h-14 mb-5"
+        >
+          Add to cart
+        </button>
+      </div>
+    </section>
+  );
 }
