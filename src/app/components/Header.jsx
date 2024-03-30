d4cba664881d0aa79ce3aa9600b50b74b359f12d
oechristophers <oechristophers2020@gmail.com>
2024-03-30 14:00:59 +0100

    fix

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,43 +3,40 @@ import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 
 export default function Header() {
-  const isClientSide = typeof window !== "undefined";
   const [isHomePage, setIsHomePage] = useState(
-    window.location.pathname === "/"
+    typeof window !== "undefined" && window.location.pathname === "/"
   );
   const [isTransparent, setIsTransparent] = useState(isHomePage);
 
   useEffect(() => {
     const handleLocationChange = () => {
-      setIsHomePage(window.location.pathname === "/" );
+      setIsHomePage(
+        typeof window !== "undefined" && window.location.pathname === "/"
+      );
     };
 
     const handleScroll = () => {
       const scrollTop =
-        window.scrollY || document.documentElement.scrollTop;
+        typeof window !== "undefined" &&
+        (window.scrollY || document.documentElement.scrollTop);
       setIsTransparent(scrollTop === 0 && isHomePage);
     };
 
-    window.addEventListener("scroll", handleScroll);
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", handleScroll);
+      window.addEventListener("popstate", handleLocationChange);
+      document.body.addEventListener("click", handleLinkClick);
 
-    // Subscribe to changes in location (back/forward clicks)
-    window.addEventListener("popstate", handleLocationChange);
-
-    // Subscribe to link clicks
-    document.body.addEventListener("click", handleLinkClick);
-
-    // Unsubscribe when component unmounts
-    return () => {
-      window.removeEventListener("popstate", handleLocationChange);
-      document.body.removeEventListener("click", handleLinkClick);
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+        window.removeEventListener("popstate", handleLocationChange);
+        document.body.removeEventListener("click", handleLinkClick);
+      };
+    }
+  }, [isHomePage]);
 
   const handleLinkClick = (event) => {
-    // Check if the clicked element is a link
     if (event.target.tagName === "A") {
-      // Check if it's an internal link
       const href = event.target.getAttribute("href");
       if (href && href.startsWith("/") && !href.startsWith("//")) {
         setIsHomePage(href === "/");
@@ -50,7 +47,9 @@ export default function Header() {
   return (
     <div
       className={` max-w-[100%] sticky top-0 z-10 headert ${
-        isHomePage && isTransparent  ? "header_main-page  text-white hover:text-black" : "bg-white"
+        isHomePage && isTransparent
+          ? "header_main-page  text-white hover:text-black"
+          : "bg-white"
       }`}
     >
       <NavBar />
