ff4d86d3c28536458aebd3ffa8dcc02cd470906e
oechristophers <oechristophers2020@gmail.com>
2024-03-30 19:16:17 +0100

    fix

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -111,7 +111,7 @@ export default function Main() {
           >
             <div className=" relative ">
               <Image
-                src="/frontimage4.jpg"
+                src="/frontimage.jpg"
                 layout="responsive"
                 width={400}
                 height={400}
